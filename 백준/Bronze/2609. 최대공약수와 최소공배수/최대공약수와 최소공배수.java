3e8902ab7b37d46d66c7c40ec3d05341ed318e58
NJWonE <93060795+NJWonE@users.noreply.github.com>
2022-12-24 20:23:44 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int[] numArray = {a,b};
+        Arrays.sort(numArray);
+        int gcd = greatestCommonDivisor(numArray[0], numArray[1]);
+        System.out.println(gcd);
+        System.out.println(greatestCommonMultiple(numArray[0],numArray[1], gcd));
+    }
+
+    public int greatestCommonDivisor(int a, int b){
+        if(b%a==0){
+            return a;
+        }else{
+            return greatestCommonDivisor(b%a, a);
+        }
+    }
+
+    public int greatestCommonMultiple(int a, int b, int c){
+        return a*b/c;
+    }
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+}
