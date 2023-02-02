3743a24553aec794a7801fdfda39fb279076f593
NJWonE <93060795+NJWonE@users.noreply.github.com>
2023-02-02 21:47:32 +0900

    [Silver I] Title: 카드 구매하기, Time: 268 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    int[] p;
+    int[] dp;
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        p = new int[n+1];
+        dp = new int[n+1];
+        for(int i=1; i<=n; i++){
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        for(int i=1; i<=n; i++){
+            dp[i] = p[i];
+            for(int j=1; j<=i-1; j++){
+                 for(int k=i-1; k>=j; k--){
+                     if(j+k==i){
+                         dp[i] = Math.max(dp[i], dp[j]+dp[k]);
+                     }
+                 }
+            }
+        }
+        System.out.println(dp[n]);
+    }
+    public static void main(String args[]) throws IOException {
+        new Main().solution();
+    }
+}
